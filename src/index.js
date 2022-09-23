169085e07ef159b03a35cb8f8b1c071d5eff3a49
Ruben S. Garcia <rssanjuan2704@gmail.com>
2022-09-23 13:54:49 +0300

    Handle possible errors

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-import { calculate } from "./js/parser.js";
-import "./styles/index.scss";
+import { calculate } from './js/parser.js';
+import './styles/index.scss';
 
 const toggleThemeClass = (element, theme) => {
   const body = document.querySelector(element);
@@ -12,20 +12,20 @@ const toggleThemeClass = (element, theme) => {
   }
 
   switch (theme) {
-    case "theme-1":
+    case 'theme-1':
       body.classList.toggle(`theme-default`);
       break;
-    case "theme-2":
+    case 'theme-2':
       body.classList.toggle(`theme-light`);
       break;
-    case "theme-3":
+    case 'theme-3':
       body.classList.toggle(`theme-dark`);
   }
 };
 
 const changeThemeHandler = (evt) => {
-  if (evt.target.type === "radio") {
-    const themeCircle = document.querySelector(".circle");
+  if (evt.target.type === 'radio') {
+    const themeCircle = document.querySelector('.circle');
     const previousActiveClassRegex = /circle-+/i;
     const previousActiveClass = [...themeCircle.classList.values()].filter(
       (cls) => previousActiveClassRegex.test(cls)
@@ -35,30 +35,49 @@ const changeThemeHandler = (evt) => {
     }
 
     themeCircle.classList.toggle(`circle--${evt.target.id}`);
-    toggleThemeClass("body", evt.target.id);
+    toggleThemeClass('body', evt.target.id);
+  }
+};
+
+const removePreviousErrors = (element) => {
+  const previousActiveClassRegex = /has-errors/i;
+  const previousActiveClass = [...element.classList.values()].filter((cls) =>
+    previousActiveClassRegex.test(cls)
+  );
+  if (previousActiveClass.length > 0) {
+    element.classList.remove(previousActiveClass);
   }
 };
 
 const calculatorPadHandler = (evt) => {
   const { value } = evt.target;
   switch (value) {
-    case "calc": {
-      const display = document.querySelector("#display");
-      const exp = display.value;
-      if (exp.length === 0) return;
-      const result = calculate(exp);
-      display.value = result;
+    case 'calc': {
+      const displayWrapper = document.querySelector('.display');
+      const display = document.querySelector('#display');
+      try {
+        const exp = display.value;
+        if (exp.length === 0) return;
+        const result = calculate(exp);
+        removePreviousErrors(displayWrapper);
+        display.value = result;
+        return;
+      } catch (err) {
+        displayWrapper.classList.toggle('display--has-errors');
+        return;
+      }
+    }
+
+    case 'clear': {
+      const displayWrapper = document.querySelector('.display');
+      const display = document.querySelector('#display');
+      display.value = '';
+      removePreviousErrors(displayWrapper);
       return;
     }
 
-    case "clear": {
-      const display = document.querySelector("#display");
-      display.value = "";
-      return;
-    }
-
-    case "del": {
-      const display = document.querySelector("#display");
+    case 'del': {
+      const display = document.querySelector('#display');
       const value = display.value;
       display.value = value.slice(0, -1);
       return;
@@ -66,17 +85,36 @@ const calculatorPadHandler = (evt) => {
 
     default: {
       if (value) {
-        const display = document.querySelector("#display");
+        const display = document.querySelector('#display');
         display.value += value;
       }
     }
   }
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-  const theme = document.querySelector(".theme__selector");
-  const calculatorPad = document.querySelector(".calculator__pad");
+const onKeyupHandler = (evt) => {
+  if (evt.code === 'Enter') {
+    const displayWrapper = document.querySelector('.display');
+    const display = document.querySelector('#display');
+    try {
+      const exp = display.value;
+      if (exp.length === 0) return;
+      const result = calculate(exp);
+      displayWrapper.classList.toggle('display--has-errors');
+      display.value = result;
+      return;
+    } catch (err) {
+      displayWrapper.classList.toggle('display--has-errors');
+    }
+  }
+};
 
-  theme.addEventListener("click", changeThemeHandler);
-  calculatorPad.addEventListener("click", calculatorPadHandler);
+document.addEventListener('DOMContentLoaded', () => {
+  const theme = document.querySelector('.theme__selector');
+  const calculatorPad = document.querySelector('.calculator__pad');
+  const display = document.querySelector('#display');
+
+  theme.addEventListener('click', changeThemeHandler);
+  calculatorPad.addEventListener('click', calculatorPadHandler);
+  display.addEventListener('keyup', onKeyupHandler);
 });
